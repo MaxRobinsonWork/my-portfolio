5b8fdf3d3bcabfafe7c420eacbebe41a2214b86d
MaxRobinsonWork <Max.Robinson.Work@gmail.com>
2024-09-28 15:48:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function showSection(sectionId) {
-    // Hide all sections
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
         section.style.display = 'none';
     });
 
-    // Show the selected section
     const selectedSection = document.getElementById(sectionId);
     if (selectedSection) {
         selectedSection.style.display = 'block';
